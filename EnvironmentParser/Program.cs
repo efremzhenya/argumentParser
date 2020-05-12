38abd7b244a484b045414a2228a5048d43901f9e
efremzhenya <efremzhenya@yandex.ru>
2020-05-12 14:05:29 +0300

    first

---
+++ EnvironmentParser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EnvironmentParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(parseArgument("asfaslhkahs {env.DOCKER_HOST} jkfhkjha {env.SomeVariable} skjh{env.USERNAME}faj"));
+        }
+        public static string parseArgument(string input) 
+        {
+            return string.IsNullOrEmpty(input) ? "" :
+                Regex.Replace(input, @"{env\.\w+}", new MatchEvaluator(GetEnvironmentValue));
+        }
+
+        private static string GetEnvironmentValue(Match match)
+        {
+            var variable = match.Value.Remove(match.Value.Length - 1, 1).Remove(0, 5);
+            return Environment.GetEnvironmentVariable(variable) ?? match.Value;
+        }
+    }
+}
